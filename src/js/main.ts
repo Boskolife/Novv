0e60ff0956cc1383df59b0bcd43b9277c036830b
Boskolife <boskoboskoboskobosko@gmail.com>
2024-02-08 05:32:25 +0200

    fix

---
+++ src/js/main.ts
@@ -0,0 +1,21 @@
+playVideo();
+
+function playVideo(): void {
+    const playBtn: HTMLElement | null = document.getElementById('play_button');
+    const video: HTMLVideoElement | null = document.getElementById('video') as HTMLVideoElement;
+
+    if (!playBtn || !video) return;
+
+    const togglePlay = () => {
+        if (video.paused) {
+            video.play();
+            playBtn.style.opacity = '0';
+        } else {
+            video.pause();
+            playBtn.style.opacity = '1';
+        }
+    };
+
+    playBtn.addEventListener('click', togglePlay);
+    video.addEventListener('click', togglePlay);
+}
